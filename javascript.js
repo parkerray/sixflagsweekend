ba3706bc33d5a18f0436a4fb68ab8bf8b2f70172
Parker <park.thompson@gmail.com>
2021-06-08 17:52:27 -0500

    added countdown functionality

---
+++ javascript.js
@@ -0,0 +1,12 @@
+const countDownDate = new Date("Sep 10, 2021 22:00:00 UTC").getTime(); //Add 5 hours for CST > UTC timezone
+const x = setInterval(function() {
+    const now = new Date().getTime();
+    const distance = countDownDate - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.querySelector("#countdown").innerHTML = `${days}d : ${hours}h : ${minutes}m : ${seconds}s`;
+}, 1000);
